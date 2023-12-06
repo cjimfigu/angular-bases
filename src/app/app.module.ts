f99b004111f0cdeebdb39cb5ffbfc565e7427cbe
ext-cjimfig <cjimenez@inerza.com>
2023-12-06 23:22:29 +0000

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { CounterModule } from './Counter/counter.module';
 import { HeoresModule } from './heroes/Heroes.module';
+import { MainPageComponent } from './dbz/pages/main-page.component';
+import { DbzModule } from './dbz/dbz.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +18,9 @@ import { HeoresModule } from './heroes/Heroes.module';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeoresModule
-  ],
+    HeoresModule,
+    DbzModule,
+     ],
   providers: [
     provideClientHydration()
   ],
